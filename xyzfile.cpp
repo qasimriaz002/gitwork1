815bb731db475697ef571ea4d7104874beb16ed0
Muhammad Qasim Riaz <qasim.riaz@giki.edu.pk>
2024-11-29 16:25:13 +0500

    Made new change
    from giki account

--- xyzfile.cpp
+++ xyzfile.cpp
@@ -7,7 +7,7 @@ int main(){
 	cout<<"Start in the name of Allah"<<endl;
 	cout<<"we are going to see changes"<<endl;
 	cout<<"I am adding a new file"<<endl;
-	
+	cout<<"Working for gik account"<<endl;
 	
 	return 0;
 }
