d45971aab5f96228f03961f3f9955c0ce2b05a9e
Muhammad Qasim Riaz <qasim.riaz002@gmail.com>
2024-11-29 15:44:17 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gitwork
+ it contains data about my semester project
---
+++ justloop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int i = 0;
+	
+	while( i < 10){
+		
+		cout<<"Value of i: "<<i<<endl;
+		i = i + 1;
+	}
+	
+	return 0;
+}
